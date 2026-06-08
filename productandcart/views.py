0a8e22382b40e2d1858da31e68dbc3a81f47213e
PushpenderRana <pushpenderrana270@gmail.com>
2026-06-08 20:54:02 +0530

    second

---
+++ productandcart/views.py
@@ -0,0 +1,16 @@
+from rest_framework.response import Response
+from rest_framework.views import APIView
+from .models import Category, Product
+from .serializers import CategorySerializer, ProductSerializer
+
+class ProductList(APIView):
+    def get(self, request):
+        products = Product.objects.all()
+        serializer = ProductSerializer(products, many=True)
+        return Response(serializer.data)
+    
+class CategoryList(APIView):
+    def get(self, request):
+        categories = Category.objects.all()
+        serializer = CategorySerializer(categories, many=True)
+        return Response(serializer.data)
